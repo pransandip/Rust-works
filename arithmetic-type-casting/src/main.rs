fb277b100d6e32da02a963fceea6d4bd2ea8b21d
pransandip <pransandip@gmail.com>
2024-11-17 13:27:29 +0530

    update:expression-statement-code

--- arithmetic-type-casting/src/main.rs
+++ arithmetic-type-casting/src/main.rs
@@ -1,3 +1,22 @@
+fn type_convertion() {
+    let m = 127_000 as i64;
+    let n = 20_i32;
+
+    let p = m / (n as i64);
+    println!("{}", p);
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let x: u8 = 9; // 0 to 255
+    // let y: i8 = 10; // -128 to 127
+    // let z = x + y;
+    // println!("{}", z);
+
+    let x = 9.5f32;
+    let y = 10.5f32;
+    let z = x + y;
+    println!("{}", z);
+
+    println!("-------typeConvertion-------");
+    type_convertion();
 }
---
+++ basic-04-project/src/main.rs
@@ -0,0 +1,20 @@
+/*
+*Function, Expressions and Statement
+*/
+
+// expression
+fn add_numbers(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+fn main() {
+    // expression
+    let number = {
+        let x = 3;
+        x + 1
+    };
+    println!("number: {}", number);
+
+    let result = add_numbers(20, 30);
+    println!("result: {}", result);
+}
