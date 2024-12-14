c16a25d3c0ec555da0b0cbc4cfff15e26dcdb596
pransandip <pransandip@gmail.com>
2024-12-14 11:55:06 +0530

    [update]:struct-proj-added

--- structs-03-project/src/main.rs
+++ structs-03-project/src/main.rs
@@ -1,3 +1,25 @@
+#[derive(Debug)]
+struct Country {
+    population: u32,
+    capital: String,
+    leader_name: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let population: u32 = 500_000;
+    let capital: String = String::from("New Delhi");
+    let leader_name = String::from("Modi");
+
+    let india = Country {
+        population,
+        capital,
+        leader_name,
+    };
+
+    println!(
+        "capital: {}, population: {}, leader nmae: {}",
+        india.capital, india.population, india.leader_name
+    );
+
+    println!("{:?}", india);
 }
