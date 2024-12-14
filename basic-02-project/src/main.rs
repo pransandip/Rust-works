1a0ee732370eb33f5e41ed886cc872f9b3fbbb88
pransandip <pransandip@gmail.com>
2024-12-14 22:19:57 +0530

    [update]:type-inference-byte-count

--- basic-02-project/src/main.rs
+++ basic-02-project/src/main.rs
@@ -1,11 +1,9 @@
 /*
-* Unsigned int can't be negative
-* By default int in rust is 32 bit
+* Rust has scaler and compound datatypes
+* By default int in rust is i32 bit
+* By default float in rust is f64 bit
 * u8: 0 to 2^8 - 1 = 0 to 255 range of numbers you can represent
 * i8: -2^7 to 2^7 - 1 = -128 to 127 range of numbers you can represent
-* By default float in rust is 64 bit
-* scaler and compound datatypes
-* tuple is a fixed lenght sequence of element that is immutable
 */
 
 fn arrays() {
@@ -24,6 +22,7 @@ fn datatypes() {
     println!("{}, {}, {}", letter, !true_or_false, floating_point);
 }
 
+// tuple is a fixed lenght sequence of element that is immutable
 fn tuples() {
     let mut tup1: (i32, bool, char) = (1, true, 'a');
     tup1.0 = 10;
@@ -31,7 +30,6 @@ fn tuples() {
 
     let tup2: (&str, u8) = ("Sandip Roy", 56);
     let (name, weight) = tup2;
-
     println!("name is: {} and weight: {}kg", name, weight)
 }
 
@@ -39,4 +37,8 @@ fn main() {
     arrays();
     tuples();
     datatypes();
+
+    //Type Inference
+    let my_num = 1_______0_______8__________u8;
+    println!("my_num: {}", my_num)
 }
